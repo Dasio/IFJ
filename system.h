101cdedbf80b541b876bbd4ba985e842ced689d1
Adam Lucansky <adamlucansky@gmail.com>
2014-08-25 18:09:12 +0200

    FEATURE: Assert import

--- system.h
+++ system.h
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <assert.h>
 
 #ifndef SYSTEM_H
 #define SYSTEM_H
+
+  // TODO Makro na malloc test alebo memory management
 
   // typedefy
 
