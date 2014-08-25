c2e681a7066ce159d01f54ecae00a5147c270a18
Adam Lucansky <adamlucansky@gmail.com>
2014-08-25 18:12:45 +0200

    DUMMY: Testing in main

--- interpret.c
+++ interpret.c
@@ -1,8 +1,12 @@
 #include "system.h"
 #include "dummy.h"
+#include "string.h"
 
 int main() {
   printf("%d\n", test());
+
+  String str = initString("Ahoj");
+  printString(&str);
 
   return 0;
 }
