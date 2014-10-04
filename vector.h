e3c037e22d461610786a67cbb24b1a5742edae11
xkello00 <kello.tomas@gmail.com>
2014-10-04 20:41:16 +0200

    Zakladne funkcie
    Funkcie Init, Append, Last, Free

--- vector.h
+++ vector.h
@@ -22,25 +22,27 @@
 	} Vector;
 
 
-#define VECTOR_INIT_SIZE 8
-
+#define VECTOR_DEFAULT_SIZE 8
 #define NewVectorType(type) \
-	GenVectorPrototypes(type)
+	GenVectorPrototypes(type) \
+	GenVectorFunctions(type)
 
 /**
  * Macro called from header file to generate appropriate prototypes
  * for define type
  * @param  type Type for generalization
  */
+
 #define GenVectorPrototypes(type) 		\
-	typedef Vector #type#Vector;
-//	#type#Vector * init#type#Vector();
+	typedef Vector type##Vector;
 
 ///////////////////////////////////
 // Definition of Vector subtypes //
 ///////////////////////////////////
-GenVectorPrototypes(Int)
-GenVectorPrototypes(Double)
+//
+//GenVectorPrototypes(int)
+//GenVectorPrototypes(double)
+//
 // ADD HERE EVERY TYPE THAT NEEDS TO HAVE *Vector
 // ALSO IN VECTOR.C
 
@@ -48,44 +50,51 @@ GenVectorPrototypes(Double)
  * Generates functions for appropriate type
  * @param  type Type for generalization
  */
+
 #define GenVectorFunctions(type) 											\
-	/** Initialized vector with initial_size, if zero, uses default */ 		\
-	#type#Vector * init#type#Vector(uint32_t initial_size) {				\
+	/** Initialized vector with initial_size, if zero, uses default (8) */ 	\
+	type##Vector * init##type##Vector(uint32_t initial_size) {				\
 		if(initial_size == 0)												\
-			initial_size = VECTOR_INIT_SIZE;								\
-		#type#Vector * tmp = malloc(sizeof(#type#Vector));					\
-		tmp->capacity = initial_size;										\
-		tmp->atomic_size = sizeof(type);									\
-		tmp->array = malloc(tmp->capacity * tmp->atomic_size);				\
-		return tmp;															\
+			initial_size = VECTOR_DEFAULT_SIZE;								\
+		type##Vector *Vect = malloc(sizeof(type##Vector));					\
+		if (Vect == NULL) printf("ERROR malloc 1\n");						\
+		Vect->used = 0;														\
+		Vect->capacity = initial_size;										\
+		Vect->atomic_size = sizeof(type);									\
+		Vect->array = malloc(Vect->capacity * Vect->atomic_size);			\
+		if (Vect->array == NULL) printf("ERROR malloc 2\n");				\
+		return Vect;														\
 	}																		\
-	/** Add Value to end ofvector */								 		\
-	void #type#VectorAppend(#type#Vector * Vect, ##type Value) {			\
-		if(Vect->capacity == Vect->atomic_size)								\
+	/** Add Value to end of vector */								 		\
+	void type##VectorAppend(type##Vector *Vect, type Value) {				\
+		if(Vect->capacity == Vect->used)									\
 		{																	\
 			Vect->capacity *= 2;											\
 			realloc(Vect->array, Vect->capacity * Vect->atomic_size);		\
+			if (Vect->array == NULL) printf("ERROR realloc 1\n");			\
 		}																	\
-		Vect->atomic_size++;												\
-		Vect->array[Vect->atomic_size] = Value;								\
-		return;																\
+		/* tmp - pointer on last Value */									\
+		type *tmp = Vect->array + Vect->used * Vect->atomic_size;			\
+		*tmp = Value;														\
+		Vect->used++;														\
 	}																		\
 	/** Return pointer to Last Value */								 		\
-	##type * #type#VectorLast(#type#Vector * Vect) {						\								\
-		return Vect->array + Vect->capacity * Vect->atomic_size;			\
+	type *type##VectorLast(type##Vector *Vect) {							\
+		return Vect->array + (Vect->used-1) * Vect->atomic_size;			\
 	}																		\
 	/** Free array from Vector */									 		\
-	#type#VectorFree(#type#Vector * Vect) {									\
+	void type##VectorFree(type##Vector *Vect) {								\
 		free(Vect->array);													\
+		free(Vect);															\
 	}
 
 
-// #type#VectorAt(uint32_t, #type#Vector *);
+// ##type##VectorAt(uint32_t, ##type##Vector *);
 // Kandidati na makro/funkciu
 // 
-// #type#VectorFirst(#type#Vector *);
-// #type#VectorBack(#type#Vector *);
-// #type#VectorSize(#type#Vector *);
+// ##type##VectorFirst(##type##Vector *);
+// ##type##VectorBack(##type##Vector *);
+// ##type##VectorSize(##type##Vector *);
 
 // ! Neskor prirobit makra, aby sa dalo volat append(tokenVector, token);
 // Ukazka generickeho makra
