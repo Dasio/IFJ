ece62fec54912f96aa39f74f5bb4ddc0458b5f89
Adam Lučanský <adamlucansky@gmail.com>
2014-10-27 20:08:52 +0100

    Playground: Scanner now reads from testFile.txt

--- playground/scanner_test.c
+++ playground/scanner_test.c
@@ -5,23 +5,31 @@ int main()
 {
 	Scanner scanner = initScanner();
 
-	assignString(&scanner.input, "ahoj_svet : := 00000123\n");
+	//assignString(&scanner.input, "ahoj_svet : := 00000123\n");
+	assignFile(&scanner.input, "testFile.txt");
 
 	Token t;
 	TokenVector *tokvect = initTokenVector(0);
 	while(true) {
 		t = getToken(&scanner);
 
+		if(getError()) {
+			printError();
+			// In case of incomplete token; must be also free'd
+			TokenVectorAppend(tokvect, t);
+			break;
+		}
+
+		tokenInfo(&t);
 		if(t.type == TT_empty || scannerFinished(&scanner))
 			break;
 
 		TokenVectorAppend(tokvect, t);
-
-		tokenInfo(&t);
 	};
 
-	destroyToken(&t);
 	destroyScanner(&scanner);
-	//printf("File closed\n");
+
+	// TODO: Destroy TokenVector
+
 	return 0;
 }
