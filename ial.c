c37973285854c38dd5809af63c66a969f6bbbb09
xkello00 <kello.tomas@gmail.com>
2014-11-02 17:24:51 +0100

    HASH table + update for quicksort
    Added functions for HASH table + vector has changed so quicksort have to
    be updated

--- ial.c
+++ ial.c
@@ -1,22 +1,22 @@
 #include "ial.h"
 
 /*
-	Find substring in string
-	original input->data
-	looking for find->data
-	
-	REQUIRED
-		both input structure have to contain LENGTH of data
-	Return
-	  SUCCESSFUL
-		index which point to input index (start from "0")
-	  UNSUCCESSFUL
-	  	length of input length
+    Find substring in string
+    original input->data
+    looking for find->data
+    
+    REQUIRED
+        both input structure have to contain LENGTH of data
+    Return
+      SUCCESSFUL
+        index which point to input index (start from "0")
+      UNSUCCESSFUL
+        length of input length
  */
 
 int FindString(String *input, String *find)
 {
-	int *Mask = GetFormula(find);
+    int *Mask = GetFormula(find);
     uint32_t inInd=1, fiInd=1;
     while(inInd<=input->length && fiInd<=find->length)
     {
@@ -43,54 +43,54 @@ int FindString(String *input, String *find)
 
 
 /*
-	Imput is string and it make Mask for KMP algorithm
-	required LENGTH in String structure
+    Imput is string and it make Mask for KMP algorithm
+    required LENGTH in String structure
  */
 int *GetFormula(String *find)
 {
-	int r;
-	int *Mask = malloc((find->length) * sizeof(int));
-	if(Mask==NULL) return 0;
-	Mask[0] = 0;
-	for (uint32_t i = 1; i < find->length; ++i)
-	{
-		r = Mask[i-1];
-		while( (r>0) && (find->data[r-1] != find->data[i-1]) )
-			r=Mask[r-1];
-		Mask[i] = r+1;
-	}
-	return Mask;
+    int r;
+    int *Mask = malloc((find->length) * sizeof(int));
+    if(Mask==NULL) return 0;
+    Mask[0] = 0;
+    for (uint32_t i = 1; i < find->length; ++i)
+    {
+        r = Mask[i-1];
+        while( (r>0) && (find->data[r-1] != find->data[i-1]) )
+            r=Mask[r-1];
+        Mask[i] = r+1;
+    }
+    return Mask;
 }
 
 /*
-	Imput is unordered array of char ended with character == 0
-	sort that array based on ASCII number.
-	
-	used Recursive Quick Sort
+    Imput is unordered array of char ended with character == 0
+    sort that array based on ASCII number.
+    
+    used Recursive Quick Sort
  */
 
 // get length of string and call QuickSortRecursive function
 void QuickSort(char arr[])
 {
-	QuickSortRecursive(arr, strlen(arr)-1);
+    QuickSortRecursive(arr, strlen(arr)-1);
 }
 
 //QuickSort which use recursive function
 void QuickSortRecursive(char *arr, int n)
 {
-	if(n<=1)
-		return;// if arr include only one number
+    if(n<=1)
+        return;// if arr include only one number
 
-	char *l=arr, *r=arr+n-1; //prepare index on border of group 
+    char *l=arr, *r=arr+n-1; //prepare index on border of group 
     char p = arr[n/2]; //set pivot (center value)
 
     while (l <= r) { // while left and right border finished
         if (*l < p) l++; //skip left number if is smaller 
         else if (*r > p) r--; //skip right number if is smaller
         else {
-        	if (l!=r) //if the numbers are different change their value
+            if (l!=r) //if the numbers are different change their value
             {
-            	*l += *r;
+                *l += *r;
                 *r = *l - *r;
                 *l -= *r;
             }
@@ -99,20 +99,20 @@ void QuickSortRecursive(char *arr, int n)
     }
 
     //recursive call for array on left and right side from the pivot
-	QuickSortRecursive(arr, r-arr+1);
-	QuickSortRecursive(l, arr+n-l);
+    QuickSortRecursive(arr, r-arr+1);
+    QuickSortRecursive(l, arr+n-l);
 }
 
 
 /*
-	Imput is unordered array of char ended with character == 0
-	sort that array based on ASCII number.
-	
-	used nonrecursive Quick Sort
+    Imput is unordered array of char ended with character == 0
+    sort that array based on ASCII number.
+    
+    used nonrecursive Quick Sort
  */
 void QuickSortNonRecursive(char arr[])
 {
-    Vector *vec = initintVector(8);
+    intVector *vec = intInitVector(8);
     int r,l,i,j;
     intVectorAppend(vec, 0); //left border of sorting
     intVectorAppend(vec, strlen(arr)-1); //rights border of sorting
@@ -127,7 +127,7 @@ void QuickSortNonRecursive(char arr[])
             int p = (i+j)/2; //set pivot (center value)
             do
             {
-            	// find higher value of pivot on left side
+                // find higher value of pivot on left side
                 while ((arr[i] <= arr[p]) & (i!=p)) i++;
                 //find lower value of pivot on right side
                 while ((arr[j] >= arr[p]) & (j!=p)) j--;
@@ -144,4 +144,87 @@ void QuickSortNonRecursive(char arr[])
         }
     }
     intVectorFree(vec);
+}
+
+
+// Calculate index for HashFunction
+unsigned int GetHash(const char *str, unsigned htab_size)
+{
+    unsigned int h=0;
+    const unsigned char *p;
+    for(p=(const unsigned char*)str; *p!='\0'; p++)
+        h = 65599*h + *p;
+    return h % htab_size;
+}
+
+
+SymbolTable *SymbolFind(Context *FunCont, char *name)
+{
+    if (FunCont==NULL || name==NULL) return NULL;
+    /* Vypocitanie indexu kde je kluc ulozeny a ulozenie prveho listu*/
+    SymbolTable *item;
+    item = FunCont->LocTable[GetHash(name,FunCont->LocCount)];
+    for(; item != NULL; item=item->next)
+        if(strcmp(item->data.name.data, name) == 0)
+            break;
+    return item;
+}
+
+SymbolTable *SymbolAdd(Context *FunCont, SymbolType type, int index, char *name, Context *SymbolContext)
+{
+    if (SymbolFind(FunCont, name) != NULL)
+    {
+        // Symbol already exist
+        setError(ERR_Unknown);
+        return NULL;
+    }
+    SymbolTable *newItem = malloc(sizeof(SymbolTable));
+    if (newItem == NULL)
+    {
+        setError(ERR_Allocation);
+        return NULL;
+    }
+    // fill newItem
+    newItem->data.type = type;
+    newItem->data.index = index;
+    newItem->data.name = initString(name);
+    newItem->data.FunCont = SymbolContext;
+
+    // reuse index variable
+    index = GetHash(name, FunCont->LocCount);
+
+    // save newItem to first position of hashTable[index]
+    newItem->next = FunCont->LocTable[index];
+    FunCont->LocTable[index] = newItem;
+    return newItem;
+}
+
+//DONE
+void ContextLocTableFree(Context *t)
+{
+    if (t==NULL) return;
+    if (t->LocTable == NULL) return;
+    SymbolTable *item = NULL;
+    SymbolTable *temp = NULL;
+    for (unsigned long i = 0; i < t->LocCount; i++)
+    {
+        if (t->LocTable[i]==NULL)
+            continue;
+        for(item=t->LocTable[i];item != NULL;item=temp)
+        {
+            temp=item->next;
+            SymbolTableFree(item);
+        }
+        t->LocTable[i] = NULL;
+    }
+    free(t->LocTable);
+    t->LocTable = NULL;
+}
+
+void SymbolTableFree(SymbolTable *t)
+{
+    if (t==NULL) return;
+    destroyString(&t->data.name);
+    free(t);
+    t = NULL;
 }
--- ial.h
+++ ial.h
@@ -6,16 +6,40 @@
 
 #include "system.h"
 #include "string.h"
-#include "vector.c"
+#include "vector.h"
+#include "symbol.h"
 
 #ifndef ial_H
 #define ial_H
 
+//struct SymbolTableStruct;
+//typedef struct SymbolTableStruct SymbolTable;
+
+
 int FindString(String *in, String *substring);
 int *GetFormula(String*);
 
-void QuickSortNonRecursive(char []);
+// Use QuickSortRecursive (faster)
 void QuickSort(char []);
+
+void QuickSortNonRecursive(char []);
 void QuickSortRecursive(char *, int);
+
+// Return hash depend on char* and (uint)length
+unsigned int GetHash(const char*, unsigned);
+
+// Free LocTable, use SymbolTableFree function
+void ContextLocTableFree(Context*);
+
+// Free SymbolTable and included String
+void SymbolTableFree(SymbolTable*);
+
+// FindSymbol in Context, return his address/NULL if failed
+SymbolTable *SymbolFind(Context *, char *);
+
+// SymbolAdd if error or symbol already exist return NULL and set Err to ERR_Unknown
+SymbolTable *SymbolAdd(Context*, SymbolType, int, char*, Context*);
+
+
 
 #endif
