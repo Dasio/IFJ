de94a9921edf365e35f3253f006835784ce3aeb2
xkello00 <kello.tomas@gmail.com>
2014-11-02 17:28:36 +0100

    +Symbol, Vector
    Added Symbol and Vector tests

--- unit_tests/test.c
+++ unit_tests/test.c
@@ -5,11 +5,15 @@ uint8_t testSuitesCount=0;
 uint8_t testPassed = 0, testFailed = 0;
 extern void stringTesting();
 extern void IALTest();
+extern void SYMBOLtest();
+extern void VECTORtest();
 
 int main()
 {
 	ADD_TEST_SUITE(stringTesting);
 	ADD_TEST_SUITE(IALTest);
+	ADD_TEST_SUITE(SYMBOLtest);
+	ADD_TEST_SUITE(VECTORtest);
 
 	printf("Starting %d test suites \n",testSuitesCount);
 	for(uint8_t i=0;i<testSuitesCount;i++)
