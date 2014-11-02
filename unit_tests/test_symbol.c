3cba97eeda6e4fe9e2ad5fdf246ba6cf3386e094
xkello00 <kello.tomas@gmail.com>
2014-11-02 17:30:36 +0100

    Created test for Symbol
    Some tests for symbol.c

---
+++ unit_tests/test_symbol.c
@@ -0,0 +1,17 @@
+#include "test.h"
+#include "../symbol.h"
+#include "../ial.h"
+
+START_TEST_SUITE(SYMBOLtest)
+	Context *Cont1 = InitContext(2);
+	AddArgToContext(Cont1, T_int, 2, "variable", NULL);
+	AddArgToContext(Cont1, T_int, 8, "variable2", NULL);
+
+	TEST_EXPR("Number of ARGument", Cont1->ArgCount == 2);
+	TEST_EXPR("Name of ARGument", equalsStringChars(&Cont1->arg[1]->data.name, "variable2"));
+	TEST_EXPR("Find symbol", SymbolFind(Cont1, "variable"));
+
+	FreeContext(Cont1);
+
+	
+END_TEST_SUITE
