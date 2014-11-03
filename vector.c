d917a8c46e0e25c43daf69b376fb632a1365744b
xkello00 <kello.tomas@gmail.com>
2014-11-03 11:34:15 +0100

    Split functions for vect of STRUCT and vect VALUE
    General functions return pointers
    + for vector of values (int, double, bool...) can use more functions
    with return of VALUE directly

--- vector.c
+++ vector.c
@@ -9,114 +9,116 @@
  * Moved to vector.c, used only for generating
  * within same module.
  * @param  type Type for generalization
+ * 
+ * GenVectorFunctions(int)
+ * for vector of structure and pointers
+ * returns ONLY pointers
+ * 
+ * GenVectorFunctionsValues(int)
+ * added functions which return directly values
+ * 
  */
 #define GenVectorFunctions(type) 											\
 	type##Vector * type##InitVector(uint32_t initial_size) {				\
 		if(initial_size == 0)												\
 			initial_size = VECTOR_DEFAULT_SIZE;								\
 		type##Vector *Vect = malloc(sizeof(type##Vector));					\
-		if (Vect == NULL)		                            				\
-        {					                                            	\
-            setError(ERR_Allocation)				                		\
-            return NULL;					                            	\
-        }					                                            	\
+		if (Vect == NULL)													\
+			return NULL;													\
 		Vect->used = 0;														\
 		Vect->capacity = initial_size;										\
-		Vect->array = malloc(Vect->capacity * sizeof(type));			    \
-		if (Vect->array == NULL)					                        \
-        {					                                            	\
-            setError(ERR_Allocation)			                           	\
-            free(Vect);					                                    \
-            return NULL;					                                \
-        }					                                            	\
-		return Vect;														\
-	}																		\
-	void type##VectorAppend(type##Vector *Vect, type Value) {				\
-		if(Vect->capacity == Vect->used)									\
+		Vect->array = malloc(Vect->capacity * sizeof(type));				\
+		if (Vect->array == NULL)											\
 		{																	\
-			Vect->capacity *= 2;											    \
-			Vect->array = realloc(Vect->array, Vect->capacity * sizeof(type));	\
-			if (Vect->array == NULL)										    \
-            {										                       	\
-                setError(ERR_Allocation)									\
-                return;										            	\
-            }				                                                \
-		}																	\
-		/* tmp - pointer on last Value */									\
-		type *tmp;                                                          \
-		tmp = (type*)Vect->array + Vect->used;   		                    \
-		*tmp = Value;														\
-		Vect->used++;														\
-	}																		\
-	void type##VectorPushMore(type##Vector *Vect, uint32_t number) {	    \
-		while(Vect->capacity < Vect->used+number)							\
-		{																	\
-			Vect->capacity *= 2;											    \
-			Vect->array = realloc(Vect->array, Vect->capacity * sizeof(type));	\
-			if (Vect->array == NULL)								            \
-            {								                                \
-                setError(ERR_Allocation)				                    \
-                return;								                        \
-            }							                	                \
-		}																	        \
-		memset((void*)((type*)Vect->array + Vect->used), 0, number*sizeof(type));	\
-		/* Set used values */												        \
-		Vect->used += number;												\
-	}																		\
-	type *type##VectorPop(type##Vector *Vect) {								\
-		if(Vect->used < 1)													\
-		{																	\
-			setError(ERR_Unknown);											\
+			free(Vect);														\
 			return NULL;													\
 		}																	\
+		return Vect;														\
+	}																		\
+	bool type##VectorAppend(type##Vector *Vect, type Value) {				\
+		if(Vect->capacity == Vect->used)									\
+		{																	\
+			Vect->capacity *= 2;												\
+			Vect->array = realloc(Vect->array, Vect->capacity * sizeof(type));	\
+			if (Vect->array == NULL)											\
+				return 0;													\
+		}																	\
+		/* tmp - pointer on last Value */									\
+		type *tmp;															\
+		tmp = (type*)Vect->array + Vect->used;								\
+		*tmp = Value;														\
+		Vect->used++;														\
+		return 1;															\
+	}																		\
+	bool type##VectorPushMore(type##Vector *Vect, uint32_t number) {		\
+		while(Vect->capacity < Vect->used+number)							\
+		{																	\
+			Vect->capacity *= 2;												\
+			Vect->array = realloc(Vect->array, Vect->capacity * sizeof(type));	\
+			if (Vect->array == NULL) return 0;									\
+		}																			\
+		memset((void*)((type*)Vect->array + Vect->used), 0, number*sizeof(type));	\
+		/* Set used values */														\
+		Vect->used += number;												\
+		return 1;															\
+	}																		\
+	type *type##VectorPop(type##Vector *Vect) {								\
+		if(Vect->used < 1) return NULL;										\
 		Vect->used--; /* Mark last value as invalid and return its address*/\
 		return (type*)Vect->array + Vect->used;								\
 	}																		\
-	type type##VectorPopValue(type##Vector *Vect) {							\
-		if(Vect->used < 1)													\
-		{																	\
-			setError(ERR_Unknown);											\
-			/*return (type)0;*/												    \
-		}																	\
-		Vect->used--; /* Mark last value as invalid and return its address*/\
-        type *r = (type*)Vect->array + Vect->used;				\
-		return *r;												            \
-	}																		\
 	type *type##VectorPopMore(type##Vector *Vect, uint32_t number) {		\
-		if(Vect->used < number)												\
-		{																	\
-			setError(ERR_Unknown);											\
-			return NULL;													\
-		}																	            \
+		if(Vect->used < number) return NULL;											\
 		Vect->used -= number; /* Mark last X value as invalid and return last address*/ \
-		return (type*)Vect->array + Vect->used;								            \
+		return (type*)Vect->array + Vect->used;											\
 	}																		\
 	type *type##VectorFirst(type##Vector *Vect){							\
-        if(Vect->used == 0) return NULL;                                    \
-		return Vect->array;												    \
-	}                           											\
-	type type##VectorFirstValue(type##Vector *Vect){						\
-        /* if(Vect->used == 0) return (type)0; */                           \
-        type *r = Vect->array;												\
-		return *r;												            \
-	}                           											\
-	type *type##VectorLast(type##Vector *Vect) {					    	\
-        if(Vect->used == 0) return NULL;                                    \
-		return (type*)Vect->array+ (Vect->used-1);                          \
-	}																		\
-	type type##VectorLastValue(type##Vector *Vect) {					    \
-        /* if(Vect->used == 0) return (type *)0; */                         \
-        type *r = (type*)Vect->array+ (Vect->used-1);                       \
-		return *r; 									                    	\
+		if(Vect->used == 0) return NULL;									\
+		return Vect->array;													\
+	}						   												\
+	type *type##VectorLast(type##Vector *Vect) {							\
+		if(Vect->used == 0) return NULL;									\
+		return (type*)Vect->array+ (Vect->used-1);							\
 	}																		\
 	void type##VectorFree(type##Vector *Vect) {								\
 		if(Vect == NULL) return;											\
 		if(Vect->array) free(Vect->array);									\
 		free(Vect);															\
-		Vect = NULL;                                                        \
+		Vect = NULL;														\
 	}
+
+
+#define GenVectorFunctionsValues(type) 										\
+	type type##VectorPopValue(type##Vector *Vect) {							\
+		if(Vect->used < 1)													\
+		{																	\
+			setError(ERR_OutOfRange);										\
+			return (type)0;													\
+		}																	\
+		Vect->used--; /* Mark last value as invalid and return its address*/\
+		type *r = (type*)Vect->array + Vect->used;							\
+		return *r;															\
+	}																		\
+	type type##VectorFirstValue(type##Vector *Vect){						\
+		 if(Vect->used == 0) 												\
+		 {																	\
+		 	setError(ERR_OutOfRange);										\
+		 	return (type)0;													\
+		 }																	\
+		type *r = Vect->array;												\
+		return *r;															\
+	}																		\
+	type type##VectorLastValue(type##Vector *Vect) {						\
+		/* if(Vect->used == 0) return (type)0; */							\
+		type *r = (type*)Vect->array+ (Vect->used-1);						\
+		return *r;															\
+	}
+
 
 // generation functions for vector
 GenVectorFunctions(int)
+GenVectorFunctionsValues(int)
 GenVectorFunctions(double)
+GenVectorFunctionsValues(double)
 GenVectorFunctions(Token)
+//GenVectorFunctions(StackData)
--- vector.h
+++ vector.h
@@ -12,32 +12,39 @@
 #include <stdlib.h>
 #include <string.h>
 #include "token.h"
+#include "symbol.h"
 
 /*
 ____ == type of vector
 --------------------------------------
-return pointer for work with structures
-return values if structure is int, double, float...
-
-____InitVector(initial_size);
-
-____VectorAppend(*Vect, Value);
-____VectorPushMore(*Vect, Number);
-____VectorPop(*Vect);
-____VectorPopValue(*Vect);
-____VectorPopMore(*Vect, Number);
-
-____VectorFirst(*Vect);
-____VectorFirstValue(*Vect);
-____VectorLast(*Vect);
-____VectorLastValue(*Vect);
-
-____VectorFree(*Vect);
+* GenVectorPrototypes(type)	
+* return pointer for work with structures or values
+* 
+* ____InitVector(initial_size);
+* 
+* ____VectorAppend(*Vect, Value);
+* ____VectorPushMore(*Vect, Number);
+* ____VectorPop(*Vect);
+* ____VectorPopMore(*Vect, Number);
+* 
+* ____VectorFirst(*Vect);
+* ____VectorLast(*Vect);
+* 
+* ____VectorFree(*Vect);
+* 
+*
+* GenVectorPrototypesValues(type)
+* return ONLY values if structure is int, double, float...
+* 
+* ____VectorPopValue(*Vect);
+* ____VectorFirstValue(*Vect);
+* ____VectorLastValue(*Vect);
+* 
 */
 #define VECTOR_DEFAULT_SIZE 8
 
 struct Vector{
-    /** Allocated elements, including uninitialized */
+	/** Allocated elements, including uninitialized */
 	uint32_t capacity;
 
 	/** Used elements */
@@ -47,58 +54,70 @@ struct Vector{
 	void *array;
 };
 
-#define GenVectorPrototypes(type) 	                        	\
-	typedef struct Vector type##Vector;	                    	\
+/** if function failed, NULL poiter (or FALSE) is returned */
+#define GenVectorPrototypes(type)								\
+	typedef struct Vector type##Vector;							\
 	\
 	\
-	/** Initialized vector with initial_size, if zero, uses default (8) */ 	\
-	/** ERR_Allocation and return NULL */ 	                    \
+	/** Initialized vector with initial_size*/					\
+	/** if zero, uses default (8) */							\
+	/** error if malloc failed: return NULL */ 					\
 	type##Vector * type##InitVector(uint32_t initial_size); 	\
 	\
 	/** Add Value to end of vector - ERR_Allocation */			\
-	/** ERR_Allocation */ 	                                    \
-	void type##VectorAppend(type##Vector *Vect, type Value);  	\
+	/** error if realloc failed: return 0 */					\
+	bool type##VectorAppend(type##Vector *Vect, type Value);	\
 	\
 	/** Reserve space in Vector */								\
-	/** error malloc failed: return NULL + ERR_Unknown */       \
-	void type##VectorPushMore(type##Vector *Vect, uint32_t);	\
+	/** error if malloc failed: return 0 */						\
+	bool type##VectorPushMore(type##Vector *Vect, uint32_t);	\
 	\
-	/** Return pointer to POP Value */     						\
-	/** error if empty: return NULL */                          \
+	/** Return pointer to POP Value */							\
+	/** error if empty: return NULL */							\
 	type *type##VectorPop(type##Vector *Vect);					\
 	\
-	/** Return POP Value */        								\
-	/** error if empty: return NULL */                          \
-	type type##VectorPopValue(type##Vector *Vect);				\
+	/** delete more items */									\
+	/** error if not enought values: return NULL */				\
+	type *type##VectorPopMore(type##Vector *Vect, uint32_t);	\
 	\
-	/** delete more items */        							\
-	/** error if not enought values: return NULL + ERR_Unknown */\
-	type *type##VectorPopMore(type##Vector *Vect, uint32_t);    \
-	\
-	/** Return pointer to first Value */        				\
-	/** error if empty: return NULL */                          \
+	/** Return pointer to first Value */						\
+	/** error if empty: return NULL */							\
 	type *type##VectorFirst(type##Vector *Vect);				\
 	\
-	/** Return first Value */        							\
-	/** error if empty: return NULL */                          \
-	type type##VectorFirstValue(type##Vector *Vect);			\
-	\
-	/** Return pointer to Last Value */        					\
-	/** error if empty: return NULL */                          \
+	/** Return pointer to Last Value */							\
+	/** error if empty: return NULL */							\
 	type *type##VectorLast(type##Vector *Vect);					\
-	\
-	/** Return last Value */        					        \
-	/** error if empty: return NULL */                          \
-	type type##VectorLastValue(type##Vector *Vect);				\
 	\
 	/** Free array from Vector */								\
 	void type##VectorFree(type##Vector *Vect);
 
+
+/** Important to chack ERRORs if value is valid */
+#define GenVectorPrototypesValues(type)							\
+	\
+	/** Return last Value */									\
+	/** error if empty: return 0 + setError */					\
+	type type##VectorLastValue(type##Vector *Vect);				\
+	\
+	/** Return first Value */									\
+	/** error if empty: return 0 + setError */					\
+	type type##VectorFirstValue(type##Vector *Vect);			\
+	\
+	/** Return POP Value */										\
+	/** error if empty: return 0 + setError */					\
+	type type##VectorPopValue(type##Vector *Vect);
+
+
+
+
+
 // generate prototypes for funciton
 
 GenVectorPrototypes(int)
+GenVectorPrototypesValues(int)
 GenVectorPrototypes(double)
+GenVectorPrototypesValues(double)
 GenVectorPrototypes(Token)
+//GenVectorPrototypes(StackData)
 
-//GenVectorPrototypes(DATA)
 #endif // VECTOR_H
