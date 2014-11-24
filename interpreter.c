922e5cefe9e31ec02894f02f16b692ed205ff9c0
Adam Lučanský <adamlucansky@gmail.com>
2014-11-24 19:32:47 +0100

    Interpreter: Now with stack

--- interpreter.c
+++ interpreter.c
@@ -5,6 +5,16 @@
  */
 static InstructionVector *tape;
 
+static STACK stack;
+
 void initInterpret() {
+	// Already initialization
+	if(tape)
+		return;
+	// TODO: Test optimal initial size
+	tape = InstructionInitVector(256);
+
+	StackInit(stack);
+
 	return;
 }
