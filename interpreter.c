afe29dc635cf9eda1a02e040ef1c5f09a6da8332
Adam Lučanský <adamlucansky@gmail.com>
2014-11-24 18:01:38 +0100

    Interpreter: Initial

---
+++ interpreter.c
@@ -0,0 +1,10 @@
+#include "interpreter.h"
+
+/**
+ * "Global" tape containing instructions
+ */
+static InstructionVector *tape;
+
+void initInterpret() {
+	return;
+}
---
+++ interpreter.h
@@ -0,0 +1,16 @@
+/**
+ * Interpreter module, containing instruction tape.
+ */
+
+#include "system.h"
+#include "instruction.h"
+#include "vector.h"
+
+#ifndef _INTERPRET_H
+#define _INTERPRET_H
+	/**
+	 * Initializes instruction tape in module
+	 */
+	void initInterpret();
+
+#endif
