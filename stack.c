ca32450b9d52cb58d58c5ca94bc88e67671f3248
Adam Lučanský <adamlucansky@gmail.com>
2014-12-08 22:43:20 +0100

    Stack: stack.SP

--- stack.c
+++ stack.c
@@ -8,7 +8,7 @@ static char data_type_translation[] = { 'S', 'D', 'I', 'B', 'U' };
 static char var_type_translation[] = { 'L', 'G', 'C', 'U' };
 
 void dumpStack() {
-	uint32_t stack_length = stack.vect->used;
+	uint32_t stack_length = stack.SP;
 	StackData *first = StackDataVectorFirst(stack.vect);
 
 	fprintf(stderr, "============================\n");
@@ -17,11 +17,9 @@ void dumpStack() {
 	for(uint32_t i = 0; i < stack_length; i++) {
 		StackData *s = first+i;
 		fprintf(stderr, "%d\t", i);
-		fprintf(stderr, "%c", data_type_translation[s->data_type]);
+		fprintf(stderr, "%c ", data_type_translation[s->data_type]);
 		fprintf(stderr, "%c ",  var_type_translation[s->var_type]);
-		fprintf(stderr, "%f ",  s->double_);
-		fprintf(stderr, "%d ",  s->int_);
-		fprintf(stderr, "%d",  s->bool_);
+		fprintf(stderr, "%d ",  s->initialized);
 		fprintf(stderr, "\n");
 	}
 	fprintf(stderr, "==== BP: %lu === SP: %lu ===\n", stack.BP, stack.SP);
