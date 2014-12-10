cda3c2ab6ce99079ceea2e344a2214230a633f01
Dasio <dasiodev@gmail.com>
2014-12-10 20:11:21 +0100

    Source file for recursive function, not working

---
+++ pas/recursive.pas
@@ -0,0 +1,25 @@
+var
+    num: integer;
+    tmp: integer;
+
+function print(cislo: integer): integer;
+var
+    tmp: integer;
+begin
+    write(cislo, ''#10'');
+
+    if 0 < cislo then
+    begin
+        print := print(cislo-1)
+    end
+    else
+    begin
+        print := 0
+    end;
+
+    write(cislo, ''#10'')
+end;
+begin
+    readln(tmp);
+    tmp:=print(tmp)
+end.
